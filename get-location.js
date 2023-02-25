76c4484559d36a8c29d244bb3aaf3eaf1062f095
Anastasiia <124095866+anastasiiaIsHere@users.noreply.github.com>
2023-02-25 12:39:41 +0100

    Update and rename get-location.js to index.html

--- get-location.js
+++
@@ -1,7 +0,0 @@
-if ("geolocation" in navigator) {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    console.log("Latitude is " + position.coords.latitude + " Longitude is " + position.coords.longitude);
-  });
-} else {
-  console.log("Geolocation is not supported by this browser.");
-}
